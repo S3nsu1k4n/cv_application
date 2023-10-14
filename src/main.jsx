fa605e9ca5a8c7c4f34acefa336f4cc78a0f674b
S3nsu1k4n <saschakoeppe@gmail.com>
2023-10-14 10:27:01 +0900

    fixed some case sensitivity on component imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import General from './components/general.jsx'
+import General from './components/General.jsx'
 import WorkExperience from './components/WorkExperience.jsx'
-import Education from './components/education.jsx'
+import Education from './components/Education.jsx'
 
 import './index.css'
 
